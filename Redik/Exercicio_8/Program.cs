d04f5d476fa5a0a62fe24a8e75c7b4547c91965f
rafael7238 <rafael7238@hotmail.com>
2021-08-10 16:42:37 -0300

    novo Commit

---
+++ Redik/Exercicio_8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qtdCaracter = 0;
+            string text = Console.ReadLine();
+            foreach (var x in text)
+            {
+                qtdCaracter++;
+            }
+            Console.WriteLine($"String de tamanho {qtdCaracter}");
+           
+
+        }
+    }
+}
